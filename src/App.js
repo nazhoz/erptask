d37b2bd2b4d89930117309985bf238f3e90602a6
nazhoz <chemmalaniyas1145@gmail.com>
2023-12-15 16:53:01 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,58 +1,97 @@
 import React, { useState } from "react";
-import './Style.scss'
-import Select from 'react-select';
+import "./Style.scss";
+import Select from "react-select";
 import Popup from "./components/Popup";
-
+import delegateList from './json/DelegateList.json'
+import Customernames from './json/CustomerDifferentNames.json'
+import Customers from './json/Customer.json'
+import ServiceType from './json/Service List.json'
+import Servicenames from './json/ServiceDifferentNames.json'
 
 const App = () => {
   const [selectedValue, setSelectedValue] = useState("");
   const [selectValue, setSelectValue] = useState("");
   const [selectsValue, setSelectsValue] = useState("");
-  const [showPopup, setShowPopup] = useState(false)
-  const [isDropdownDisabled, setIsDropdownDisabled] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
+  const [tableRows, setTableRows] = useState([
+    { delegateName: "", address: "", email: "", pickup: "", drop: "", exception: "" },
+  ]);
 
   const options = ["option 1", "options 2", "options 3"];
 
   const handleDropDownChange = (event) => {
     setSelectedValue(event.target.value);
   };
+
   const handledropdownchanges = (event) => {
     setSelectValue(event.target.value);
   };
+
   const handleDropDownChanged = (event) => {
     setSelectsValue(event.target.value);
   };
 
-  // =================== table option dropdown===========================================
-  const option =[
-    {value:'option 1', label:'option 1', },
-    {value:'option 2', label:'option 2', },
-    {value:'option 3', label:'option 3', },
-  ]
+  const handleTableRowChange = (selectedOption, index, field) => {
+    const updatedRows = [...tableRows];
+    updatedRows[index][field] = selectedOption; // Store the entire option object
+  
+    // Check if the user is typing in the last row
+    if (index === updatedRows.length - 1 && selectedOption && selectedOption.value !== "") {
+      // Add a new empty row
+      updatedRows.push({ delegateName: "", address: "", email: "", pickup: "", drop: "", exception: "" });
+    }
+  
+    // Update the state with the modified rows
+    setTableRows(updatedRows);
+  };
 
   const thStyle = {
-    fontSize:'12px',
-    border: 'none',
-    outline: 'none',
-    background: 'none',
-  }
+    fontSize: "12px",
+    border: "none",
+    outline: "none",
+    background: "none",
+  };
 
-  // ================================== POPUP =======================================
+  const option = [
+    { value: "option 1", label: "option 1" },
+    { value: "option 2", label: "option 2" },
+    { value: "option 3", label: "option 3" },
+  ];
 
-  const handleAddExceptionClick = ()=>{
-    setShowPopup(true)
-    setIsDropdownDisabled(true);
-  }
+  const delegateOptions = delegateList.map((delegate) => ({
+    value: delegate.DelegateID,
+    label: delegate.DelegateName,
+  }));
+  const CustomerOptions = Customers.map((item) => ({
+    value: item.CustomerID,
+    label: item.CustomerName,
+  }));
+  const CustomerNames = Customernames.map((item) => ({
+    value: item.CustomerID,
+    label: item.Name,
+  }));
+  const serviceTypes = ServiceType.map((item) => ({
+    value: item.ID,
+    label: item.ServiceName,
+    type:item.ScheduleType
+  }));
+  const serviceNames = Servicenames.map((item) => ({
+    value: item.ID,
+    label: item.Name,
+    // type:item.ScheduleType
+  }));
 
-  const handleClosePopup = ()=>{
-    setShowPopup(false)
-    setIsDropdownDisabled(false);
-  }
+  const handleAddExceptionClick = () => {
+    setShowPopup(true);
+  };
 
-  const handleSaveException = (popup)=>{
-    console.log('Popup :', popup)
-  }
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
 
+  const handleSaveException = (popup) => {
+    console.log("Popup :", popup);
+  };
 
   return (
     <div className="App">
@@ -62,177 +101,193 @@ const App = () => {
       <div className="container">
         <div className="firstInput">
           <div className="inputField">
-          <span>Request No :</span>
-          <input type="text" placeholder="id" />
+            <span>Request No :</span>
+            <input type="text" placeholder="id" />
           </div>
           <div className="DropDowns">
-          <label htmlFor="dropdown">Customer Name :</label>
-          <select
-            id="dropdown"
-            value={selectedValue}
-            onChange={handleDropDownChange}
-          >
-            <option value="" disabled hidden>Select...</option>
-            {options.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
+            <label htmlFor="dropdown">Customer Name :</label>
+            <select id="dropdown" value={selectedValue} onChange={handleDropDownChange}>
+              <option value="" disabled hidden>
+                Select...
               </option>
-            ))}
-          </select>
-          </div>      
+              {CustomerNames.map((option, index) => (
+                <option key={index} value={option.label}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
         <div className="secondInput">
           <div className="selectedoptions">
-          <label htmlFor="dropdown">Service Type :</label>
-          <select
-            id="dropdown"
-            value={selectValue}
-            onChange={handledropdownchanges}
-          >
-            <option value="" hidden>Select...</option>
-            {options.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
+            <label htmlFor="dropdown">Service Type :</label>
+            <select id="dropdown" value={selectValue} onChange={handledropdownchanges}>
+              <option value="" hidden>
+                Select...
               </option>
-            ))}
-          </select>
+              {serviceTypes.map((option, index) => (
+                <option key={index} value={option.label}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
           </div>
-            <div className="selectedoption">
+          <div className="selectedoption">
             <label htmlFor="dropdown">Customer Types :</label>
-          <select
-            id="dropdown"
-            value={selectsValue}
-            onChange={handleDropDownChanged}
-          >
-            <option value="" hidden >Select...</option>
-            {options.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
+            <select id="dropdown" value={selectsValue} onChange={handleDropDownChanged}>
+              <option value="" hidden>
+                Select...
               </option>
-            ))}
-          </select>
-            </div>
-          
+              {CustomerOptions.map((option, index) => (
+                <option key={index} value={option.label}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
 
-      
-
-          {/*============================ FIRST TABLE ==================================== */}
-
+      {/* First Table */}
       <div className="table">
         <div className="table-head">
           <span>Service Details</span>
         </div>
 
         <div className="table-body">
-      <table>
-        <thead className="first-table-head">
-          <tr>
-            <th className="tableslno" style={thStyle}>
-              <span className="firsttableno">Sl. No</span>
-            </th>
-            <th className="TableHead" style={thStyle}>
-              <Select className="TableSelectOption" options={option} placeholder="Service Name" />
-            </th>
-            <th className="TableHead1" style={thStyle}>
-              <Select className="TableSelectOption1" options={option} placeholder="Name in Certificate" />
-            </th>
-            <th className="TableHead2" style={thStyle}>
-              <Select className="TableSelectOption2" options={option} placeholder="Requested Date Time" />
-            </th>
-            <th className="TableHead3" style={thStyle}>
-              <Select className="TableSelectOption3" options={option} placeholder="Unit" />
-            </th>
-            <th className="TableHead4" style={thStyle}>
-              <Select className="TableSelectOption4" options={option} placeholder="Qty" />
-            </th>
-            <th className="TableHead5" style={thStyle}>
-              <Select className="TableSelectOption5" options={option} placeholder="LPO Refrence No" />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Row 1</td>
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-          </tr>
-        </tbody>
-      </table>
-      {showPopup && (
-        <Popup onClose={handleClosePopup} onSave={handleSaveException}/>
-      )}
-    </div>
+          <table>
+            <thead className="first-table-head">
+              <tr>
+                <th>Sl.No</th>
+                <th>Service Name</th>
+                <th>Name in Certificate</th>
+                <th>Requested Date Time</th>
+                <th>Unit</th>
+                <th>Qty</th>
+                <th>LPO Reference No</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="tableslno1" style={thStyle}>
+                  <span className="firsttableno1">1</span>
+                </td>
+                <td className="TableHead12" style={thStyle}>
+                  <Select className="TableSelectOption12" options={serviceNames} placeholder="Service Name" />
+                </td>
+                <td className="TableHead7" style={thStyle}>
+                  <input type='text'  className="TableSelectOption7" placeholder="Name in Certificate" />
+                </td>
+                <td className="TableHead8" style={thStyle}>
+                  <input type="date" className="TableSelectOption8" placeholder="Requested Date Time" />
+                </td>
+                <td className="TableHead9" style={thStyle}>
+                  <input type='text' className="TableSelectOption9" options={option} placeholder="Unit" />
+                </td>
+                <td className="TableHead0" style={thStyle}>
+                <input type='text' className="TableSelectOption0" options={option} placeholder="Qty" />
+                </td>
+                <td className="TableHead11" style={thStyle}>
+                <input type='text'  className="TableSelectOption11" options={option} placeholder="LPO Reference No" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          {showPopup && <Popup onClose={handleClosePopup} onSave={handleSaveException} />}
+        </div>
       </div>
 
-      
-              {/*============================ SECOND TABLE ==================================== */}
-
+      {/* Second Table */}
       <div className="table">
         <div className="table-head">
           <span>Delegated Services</span>
         </div>
 
         <div className="table-body">
-      <table>
-        <thead>
-          <tr>
-            <th className="tableslno" style={thStyle}>
-              <span className="firsttableno">Sl. No</span>
-            </th>
-            <th className="TableHead" style={thStyle}>
-              <Select className="TableSelectOption" options={option} placeholder="Delegate Name" />
-            </th>
-            <th className="TableHead1" style={thStyle}>
-              <Select className="TableSelectOption1" options={option} placeholder="Address" />
-            </th>
-            <th className="TableHead2" style={thStyle}>
-              <Select className="TableSelectOption2" options={option} placeholder="Email" />
-            </th>
-            <th className="TableHead3" style={thStyle}>
-              <Select className="TableSelectOption3" options={option} placeholder="PickUp" />
-            </th>
-            <th className="TableHead4" style={thStyle}>
-              <Select className="TableSelectOption4" options={option} placeholder="Drop" />
-            </th>
-            <th className="TableHead6" style={thStyle}>
-            <Select className="TableSelectOption6" options={option} isDisabled={isDropdownDisabled} placeholder="Add Exception" />
-              <button className="tableSelectOption6btn" onClick={handleAddExceptionClick} />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Row 1</td>
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-            <td>Data 1</td>           
-          </tr>
-          
-        </tbody>
-      </table>
-    </div>
-      </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Sl.No</th>
+                <th>Delegate Name</th>
+                <th>Address</th>
+                <th>Email</th>
+                <th>PickUp</th>
+                <th>Drop</th>
+                <th>Exception</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableRows.map((row, index) => (
+                <tr key={index}>
+                  <th className="tableslno" style={thStyle}>
+                    <span className="firsttableno">{index + 1}</span>
+                  </th>
+                  <th className="TableHead" style={thStyle}>
+                  <Select
+                      className="TableSelectOption"
+                      options={delegateOptions}
+                      placeholder="Delegate Name"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'delegateName')}
+                      value={row.delegateName}
+                    />
 
+                  </th>
+                  <th className="TableHead1" style={thStyle}>
+                  <input type='text' 
+                      className="TableSelectOption1"
+                      placeholder="Address"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'address')}
+                      value={row.address}
+                    />
+                  </th>
+                  <th className="TableHead2" style={thStyle}>
+                  <input type='text' 
+                      className="TableSelectOption2"
+                      placeholder="Email"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'email')}
+                      value={row.email}
+                    />
+                  </th>
+                  <th className="TableHead3" style={thStyle}>
+                    <Select
+                      className="TableSelectOption3"
+                      options={option}
+                      placeholder="PickUp"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'pickup')}
+                      value={row.pickup}
+                    />
+                  </th>
+                  <th className="TableHead4" style={thStyle}>
+                    <Select
+                      className="TableSelectOption4"
+                      options={option}
+                      placeholder="Drop"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'drop')}
+                      value={row.drop}
+                    />
+                  </th>
+                  <th className="TableHead6" style={thStyle}>
+                    <Select
+                      className="TableSelectOption6"
+                      options={option}
+                      placeholder="Add Exception"
+                      onChange={(selectedOption) => handleTableRowChange(selectedOption, index, 'exception')}
+                      value={row.exception}
+                    />
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
 
       <div className="savebutton">
-        {/* <div className="allbuttons"> */}
-          <button>Save</button>
-          <button>Clear</button>
-          <button>Delete</button>
-          <button>Close</button>
-        {/* </div> */}
+        <button>Save</button>
+        <button>Clear</button>
+        <button>Delete</button>
+        <button>Close</button>
       </div>
-
-      
     </div>
   );
 };
